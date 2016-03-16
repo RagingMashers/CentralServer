a3d4e4fa7971ca706ba37106d6da56a3d88e4dbe
0Rick0 <rickrongen@outlook.com>
2016-03-16 15:11:29 +0100

    Initial test suite

--- CentralServer/CentralServer/sita/Person.cs
+++ CentralServer/CentralServer/sita/Person.cs
@@ -1,6 +1,29 @@
 using System;
+using System.Collections.Generic;
+
 namespace CentralServer.sita {
 	public class Person {
+        /// <summary>
+        /// Create a new instance of Person
+        /// </summary>
+        /// <param name="id">The persons id</param>
+        /// <param name="name">The persons name</param>
+        /// <param name="dayOfBirth">The birthdate of the person</param>
+        /// <param name="occupation">The occupation of the person</param>
+        /// <param name="homeLongitude">The long of the persons home address</param>
+        /// <param name="homeLatitude">The lat of the persons address</param>
+	    public Person(int id, string name, DateTime dayOfBirth, string occupation, double homeLongitude, double homeLatitude)
+	    {
+	        this.id = id;
+	        this.name = name;
+	        this.dayOfBirth = dayOfBirth;
+	        this.occupation = occupation;
+	        this.homeLongitude = homeLongitude;
+	        this.homeLatitude = homeLatitude;
+	        this.available = true;
+	        this.teams = new List<Team>();
+	    }
+
 		private readonly int id;
 		public int Id {
 			get {
@@ -62,7 +85,18 @@ namespace CentralServer.sita {
 			}
 		}
 
-		private System.Collections.Generic.List<Team> teams;
+        /// <summary>
+        /// Assign this person to a team
+        /// </summary>
+        /// <param name="team">The team to assign to</param>
+	    public void AssignToTeam(Team team)
+	    {
+	        teams.Add(team);
+	    }
+
+		private List<Team> teams;
+
+	    public IReadOnlyCollection<Team> Teams => teams.AsReadOnly();
 
 	}
 
--- CentralServer/CentralServer/sita/Team.cs
+++ CentralServer/CentralServer/sita/Team.cs
@@ -1,77 +1,137 @@
 using System;
+using System.Collections.Generic;
+
 namespace CentralServer.sita {
-	public class Team {
-		private readonly int id;
-		public int Id {
-			get {
-				return id;
-			}
-		}
-		private ServiceType type;
-		public ServiceType Type {
-			get {
-				return type;
-			}
-			set {
-				type = value;
-			}
-		}
-		private DateTime startDate;
-		public DateTime StartDate {
-			get {
-				return startDate;
-			}
-			set {
-				startDate = value;
-			}
-		}
-		private DateTime endDate;
-		public DateTime EndDate {
-			get {
-				return endDate;
-			}
-			set {
-				endDate = value;
-			}
-		}
-		private double longitude;
-		public double Longitude {
-			get {
-				return longitude;
-			}
-			set {
-				longitude = value;
-			}
-		}
-		private double latitude;
-		public double Latitude {
-			get {
-				return latitude;
-			}
-			set {
-				latitude = value;
-			}
-		}
+    public class Team
+    {
+        public Team(int id, ServiceType type, double longitude, double latitude)
+            : this(id, type, longitude, latitude, DateTime.Now, DateTime.Now.AddDays(1))
+        {
+        }
 
-		public bool AssignPerson(ref Person person) {
-			throw new System.Exception("Not implemented");
-		}
-		public bool ReleivePerson(ref Person person) {
-			throw new System.Exception("Not implemented");
-		}
-		public bool AddResource(ref Resource resource) {
-			throw new System.Exception("Not implemented");
-		}
-		public bool RemoveResource(ref Resource resource) {
-			throw new System.Exception("Not implemented");
-		}
+        public Team(int id, ServiceType type, double longitude, double latitude, DateTime startDate, DateTime endDate)
+        {
+            this.id = id;
+            this.type = type;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.longitude = longitude;
+            this.latitude = latitude;
+            this.personsInTeams = new List<Person>();
+            this.resourcesAvailableToTeam = new List<Resource>();
+        }
 
-		private System.Collections.Generic.List<Incident> teams_on_location2;
-		private ServiceType serviceType;
+        private readonly int id;
 
-		private System.Collections.Generic.List<Person> persons_in_teams;
-		private System.Collections.Generic.List<Resource> resources_available_to_team;
+        public int Id
+        {
+            get { return id; }
+        }
 
-	}
+        private ServiceType type;
+
+        public ServiceType Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        private DateTime startDate;
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+        private DateTime endDate;
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+        private double longitude;
+
+        public double Longitude
+        {
+            get { return longitude; }
+            set { longitude = value; }
+        }
+
+        private double latitude;
+
+        public double Latitude
+        {
+            get { return latitude; }
+            set { latitude = value; }
+        }
+
+        /// <summary>
+        /// Assign a person to this team
+        /// </summary>
+        /// <param name="person">The person to assign</param>
+        /// <returns>if the person is correctly added</returns>
+        public bool AssignPerson(Person person)
+        {
+            if (person == null) throw new NullReferenceException();
+            if (!person.Available || personsInTeams.Contains(person)) return false; //member already in team
+            person.Available = false; //reserverve the person
+            personsInTeams.Add(person);
+            person.AssignToTeam(this);
+            return true;
+        }
+
+        /// <summary>
+        /// Releive a person from this team.
+        /// </summary>
+        /// <param name="person">The person to releive</param>
+        /// <returns>If the persons is succesfully releived</returns>
+        public bool ReleivePerson(Person person)
+        {
+            if (person == null) throw new NullReferenceException();
+            if (!personsInTeams.Contains(person)) return false; //he isn't in the team
+            person.Available = true;
+            personsInTeams.Remove(person);
+            return true;
+        }
+
+        /// <summary>
+        /// Add a resource to this team
+        /// </summary>
+        /// <param name="resource">The resource to add</param>
+        /// <returns>If the resource is succesfully added</returns>
+        public bool AddResource(Resource resource)
+        {
+            if (resource == null) throw new NullReferenceException();
+            if (!resource.Available || resourcesAvailableToTeam.Contains(resource)) return false;
+            resource.Available = false; //reserver the person
+            resourcesAvailableToTeam.Add(resource);
+            resource.AssigntToTeam(this);
+            return true;
+        }
+
+        /// <summary>
+        /// Remove a resource from a team
+        /// </summary>
+        /// <param name="resource">The resource to add</param>
+        /// <returns>If the resource was succesfully added</returns>
+        public bool RemoveResource(Resource resource)
+        {
+            if (resource == null) throw new NullReferenceException();
+            if (!resourcesAvailableToTeam.Contains(resource)) return false;
+            resource.Available = true;
+            resourcesAvailableToTeam.Remove(resource);
+            return true;
+        }
+
+        private readonly List<Person> personsInTeams;
+
+        public IReadOnlyCollection<Person> PersonsInTeam => personsInTeams.AsReadOnly();
+
+        private readonly List<Resource> resourcesAvailableToTeam;
+        public IReadOnlyCollection<Resource> ResourcesAvailableToTeam => resourcesAvailableToTeam.AsReadOnly();
+    }
 
 }
