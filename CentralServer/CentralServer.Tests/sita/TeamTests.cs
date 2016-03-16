98a6a6efec714d7777eb3c771735a86ee7156b15
0Rick0 <rickrongen@outlook.com>
2016-03-16 13:37:23 +0100

    Created tests

---
+++ CentralServer/CentralServer.Tests/sita/TeamTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CentralServer.sita;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralServer.sita.Tests
+{
+    [TestClass()]
+    public class TeamTests
+    {
+        [TestMethod()]
+        public void AssignPersonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReleivePersonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddResourceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveResourceTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
