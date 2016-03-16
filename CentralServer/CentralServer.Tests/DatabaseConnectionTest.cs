aa5fd6c5604abcf8f359a91d2e173ff8a1de16fb
Mike Evers <mike_evers1997@hotmail.com>
2016-03-16 16:10:51 +0100

    Insert + connect + database works

---
+++ CentralServer/CentralServer.Tests/DatabaseConnectionTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CentralServer.Database;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace CentralServer.Tests
+{
+    [TestClass]
+    public class DatabaseConnectionTest
+    {
+        [TestMethod]
+        public void ConnectTest()
+        {
+            DatabaseConnection connection = new DatabaseConnection();
+            Assert.IsTrue(connection.Connect());
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            DatabaseConnection connection = new DatabaseConnection();
+            List<MySqlParameter> parameters = new List<MySqlParameter>();
+            parameters.Add(new MySqlParameter("@id", 2));
+            parameters.Add(new MySqlParameter("@amountVictims", 2));
+            parameters.Add(new MySqlParameter("@amountWounded", 3));
+            parameters.Add(new MySqlParameter("@long", 4));
+            parameters.Add(new MySqlParameter("@lat", 5));
+            parameters.Add(new MySqlParameter("@radius", 6));
+            parameters.Add(new MySqlParameter("@danger", 7));
+
+            int affectedRows = connection.Insert("INSERT INTO Incident VALUES (@id, @amountVictims ,@amountWounded, @long, @lat, @radius, @danger)", parameters);
+            Assert.AreEqual(1, affectedRows);
+        }
+    }
+}
---
+++ CentralServer/CentralServer/Database/DatabaseConnection.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace CentralServer.Database
+{
+    public class DatabaseConnection
+    {
+        private string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        private string con = "Server=athena01.fhict.local;Database=dbi318908;Uid=dbi318908;Pwd=proftaak2016;";
+        MySqlConnection connection = null;
+
+        public DatabaseConnection()
+        {
+            Connect();
+        }
+
+        
+
+        /// <summary>
+        /// Connects to the database connection.
+        /// </summary>
+        public bool Connect()
+        {
+            try
+            {
+                connection = new MySqlConnection();
+                connection.ConnectionString = constr;
+                connection.Open();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+
+        /// <summary>
+        /// Closes the database connection.
+        /// </summary>
+        public void Close()
+        {
+            if (connection != null)
+            {
+                connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Executes an query on the database and returns an string array.
+        /// </summary>
+        /// <param name="query">The query</param>
+        //public string ExecuteQuery(string query)
+        //{
+        //    try
+        //    {
+        //            if (Connect())
+        //            {
+        //                MySqlCommand cmd = connection.CreateCommand();
+        //                cmd.CommandText = query;
+
+        //                MySqlDataReader Reader = cmd.ExecuteReader();
+        //                if (!Reader.HasRows) return null;
+        //                while (Reader.Read())
+        //                {         
+        //                    return Reader["id"].ToString();
+        //                }
+        //                Reader.Close();
+        //            }
+        //    }
+        //    catch (MySqlException ex)
+        //    {
+        //        Console.WriteLine(ex.ToString());
+        //    }
+        //    finally
+        //    {
+        //        if (connection.State == ConnectionState.Open)
+        //        {
+        //            Close();
+        //        }
+        //    }
+        //    return null;
+        //}
+
+        /// <summary>
+        /// Insert data into the database
+        ///
+        /// </summary>
+        /// <param name="query">The insert query</param>
+        /// <param name="parameters">The parameters used for preventing SQL injection</param>
+        /// <returns>Returns the number of affected rows.</returns>
+        public int Insert(string query, List<MySqlParameter> parameters)
+        {
+            //try
+            //{
+                if (Connect())
+                {
+                    MySqlCommand cmd = connection.CreateCommand();
+                    cmd.CommandText = query;
+                    foreach(MySqlParameter parameter in parameters){
+                        cmd.Parameters.Add(parameter);
+                    }
+
+                    return cmd.ExecuteNonQuery();
+                }
+            //}
+            //catch (MySqlException ex)
+            //{
+            //    Console.WriteLine(ex.ToString());
+            //}
+            //finally
+            //{
+                if (connection.State == ConnectionState.Open)
+                {
+                    Close();
+                }
+            //}
+            return 0;
+        }
+    }
+}
