7c14482c13e7cbeb96a159dff0e34a367c1135c2
0Rick0 <rickrongen@outlook.com>
2016-06-01 11:42:40 +0200

    Added web api wich is capable of json for android client

--- CentralServer/CentralServer/api/Authentication.cs
+++ CentralServer/CentralServer/api/Authentication.cs
@@ -15,13 +15,30 @@ namespace CentralServer.api
         public object Login([FromUri]string username = null, [FromUri]string password = null)
         {
             object result;
+            var teamId = -1;
+            var token = "";
+            var loggedin = false;
+            var errorCode = 0;
+            var errorText = "";
             if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
             {
-                result = new {loggedin = false, token = "", errorCode = 10, errorText="Missing parameters"};
+                errorCode = 10;
+                errorText = "Missing parameters";
+                result = new {loggedin, username, token, teamId, errorCode, errorText};
+            }
+            else if (username == "testUser" && password == "testPass")
+            {
+                loggedin = true;
+                token = "testToken";
+                teamId = 2;
+                errorText = "OK!";
+                result = new { loggedin, username, token, teamId, errorCode, errorText };
             }
             else
             {
-                result = new { loggedin = false, token = "", errorCode = -1, errorText = "Not yet implemented" };
+                errorCode = -1;
+                errorText = "Not yet implemented";
+                result = new { loggedin, username, token, teamId, errorCode, errorText};
             }
             return result;
         }
@@ -30,15 +47,25 @@ namespace CentralServer.api
         public object Logout([FromUri]string username = null, [FromUri]string token = null)
         {
             object result;
+            var succes = false;
+            var errorCode = 0;
+            var errorText = "";
             if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(token))
             {
-                result = new {success = false, errorCode = 10, errorText = "Missing parameters"};
-            }else if (false)//token or username not found or not matching
+                errorCode = 10;
+                errorText = "Missing parameters";
+                result = new {succes, errorCode, errorText};
+            }else if (username == "testUser" && token == "testToken")//token or username not found or not matching
             {
-                result = new { success = false, errorCode = 1, errorText = "Username and token do not match or do not exist" };
+                succes = true;
+                errorText = "OK!";
+                result = new { succes, errorCode, errorText };//Username and token do not match or do not exist = 1
             }
-            else{
-                result = new { success = false, errorCode = -1, errorText = "Not yet implemented" };
+            else
+            {
+                errorCode = -1;
+                errorText = "Not yet implemented";
+                result = new { succes, errorCode, errorText };
             }
             return result;
         }
---
+++ CentralServer/CentralServer/api/MessagesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace CentralServer.api
+{
+    public class MessagesController : ApiController
+    {
+        public object GetMessages([FromUri]string username, [FromUri]string token)
+        {
+            var result =  new {succes = true, messages = new List<object>()};
+            var testMedia = new {id = 1, mimetype = "image/jpeg", url =$"http://{Request.RequestUri.Host+":"+Request.RequestUri.Port}/MediaDownload.ashx?id={1}", source = "Facebook", suggestion = "I have no clue", importance = 2, category = "TestCat"};
+            result.messages.Add(new { message = "Test1", title = "Test1T", team = 1, direction = "T", media = new List<object>() {testMedia} });
+            result.messages.Add(new { message = "Test2", title = "Test2T", team = 2, direction = "T", media = new List<object>() });
+            result.messages.Add(new { message = "Test3", title = "Test3T", team = 3, direction = "T", media = new List<object>()});
+            return result;
+        }
+
+        public object PostMessage([FromBody]JObject body, [FromUri]string username, [FromUri]string token)
+        {
+            //todo use body
+            return new {succes=true};
+        }
+    }
+}
